9fcabbedb24132641a046d9d19b9aa5d20efa3ba
malzak <malzovich@gmail.com>
2021-06-24 17:11:04 +0100

    list todo

--- src/components/ItemTodo.js
+++ src/components/ItemTodo.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function ItemTodo(name, description) {
     return (
-        <tr>
+        <>
             <td>{name}</td>
             <td>{description}</td>
-        </tr>
+        </>
     )
 }
 
--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -2,22 +2,44 @@ import React, { Component, Fragment } from "react";
 import ItemTodo from "./ItemTodo";
 
 export class ListTodo extends Component {
-  
+
     todosFromLocalStorage = JSON.parse(localStorage.getItem('todos') || '[]');
     state = {
         todos: this.todosFromLocalStorage
     }
-   
-    
-    
-  render() {
-    console.log(this.state.todos) ;
-    return (
-      <div className="container mt-3">
-        
-      </div>
-    );
-  }
+
+
+
+    render() {
+        // console.log(this.state.todos) ;
+        return (
+            <div className="container mt-3">
+                <table  className="table ">
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Description</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.todos.map((todo, index) => {
+                                return (
+                                    <Fragment key={index}>
+                                        <tr> 
+                                            <td>{todo.name}</td>
+                                             <td>{todo.description}</td>
+                                        </tr>
+                                    </Fragment>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default ListTodo;
