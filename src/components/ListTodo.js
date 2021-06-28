ede7f43284802b4fa56097f6e4355b718e2f9a23
malzak <malzovich@gmail.com>
2021-06-28 12:03:29 +0100

    delete funtion from list

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -2,58 +2,61 @@ import React, { Component, Fragment } from "react";
 import ItemTodo from "./ItemTodo";
 
 export class ListTodo extends Component {
+  todosFromLocalStorage = JSON.parse(localStorage.getItem("todos") || "[]");
+  state = {
+    todos: this.todosFromLocalStorage,
+  };
 
-    todosFromLocalStorage = JSON.parse(localStorage.getItem('todos') || '[]');
-    state = {
-        todos: this.todosFromLocalStorage
-    }
+  handleDelete = (index) => {
+    const todosLocalStorage = JSON.parse(localStorage.getItem("todos") || "[]");
 
-   handleDelete = (index, e) => {
-    //  todosLocalStorage = JSON.parse(localStorage.getItem('todos') || '[]');
+    todosLocalStorage.splice(index, 1);
+    console.log(todosLocalStorage);
+    localStorage.setItem("todos", JSON.stringify(todosLocalStorage));
+    this.setState({
+      todos: todosLocalStorage,
+    });
+  };
 
-    //  for (var i =0; i< todosLocalStorage.length; i++) {
-    //     var items = JSON.parse(todosLocalStorage[i]);
-    //     if (items.itemId == 3) {
-    //         items.splice(i, 1);
-    //     }
-    // }
-
-    //    localStorage.removeItem();
-       //console.log(index);
-   }
-
-    render() {
-        // console.log(this.state.todos) ;
-        return (
-            <div className="container mt-3">
-                <table  className="table">
-                    <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Description</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.todos.map((todo, index) => {
-                                return (
-                                    <Fragment key={index}>
-                                        <tr> 
-                                            <td>{todo.name}</td>
-                                             <td>{todo.description}</td>
-                                             <td><button className="btn btn-danger" onClick={this.handleDelete.bind(this, index)}>Delete</button></td>
-                                             <td><button className="btn btn-info">Detail</button></td>
-                                        </tr>
-                                    </Fragment>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+  render() {
+    // console.log(this.state.todos) ;
+    return (
+      <div className="container mt-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Description</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.todos.map((todo, index) => {
+              return (
+                <Fragment key={index}>
+                  <tr>
+                    <td>{todo.name}</td>
+                    <td>{todo.description}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={this.handleDelete.bind(index)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                    <td>
+                      <button className="btn btn-info">Detail</button>
+                    </td>
+                  </tr>
+                </Fragment>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default ListTodo;
