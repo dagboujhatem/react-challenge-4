3db156cb61ffbcf48cf02f788be845d767f4b620
dagbouj hatem <dagboujhatem@gmail.com>
2021-06-30 16:18:31 +0100

    update table in list todos

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -2,14 +2,14 @@ import React, { Component, Fragment } from "react";
 
 
 export class ListTodo extends Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
       todos: JSON.parse(localStorage.getItem("todos") || "[]"),
     };
   }
-  
+
 
   handleDelete = (index) => {
     const todosLocalStorage = JSON.parse(localStorage.getItem("todos") || "[]");
@@ -21,41 +21,45 @@ export class ListTodo extends Component {
   };
 
   handleDetails = (index) => {
-      this.props.history.push(`/detailTodo/${index}`);
+    this.props.history.push(`/detailTodo/${index}`);
   }
   render() {
     return (
       <div className="container-fuild my-5">
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.todos.map((todo, index) => {
-              return (
-                <Fragment key={index}>
-                  <tr>
-                    <td>{todo.name}</td>
-                    <td>{todo.description}</td>
-                    <td>
-                      <button
-                        className="btn btn-danger"
-                        onClick={this.handleDelete.bind(this, index)}>
-                        <i className="fa fa-trash"></i>   Delete
+        <div className="row">
+          <div className="offset-2 col-sm-8">
+            <table className="table table-striped table-hover text-center">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.todos.map((todo, index) => {
+                  return (
+                    <Fragment key={index}>
+                      <tr>
+                        <td>{todo.name}</td>
+                        <td>{todo.description}</td>
+                        <td>
+                          <button
+                            className="btn btn-danger"
+                            onClick={this.handleDelete.bind(this, index)}>
+                            <i className="fa fa-trash"></i>   Delete
                       </button>
-                        <button className="btn btn-info text-white" onClick={this.handleDetails.bind(this, index)}>
-                        <i className="fa fa-edit"></i> Details</button>
-                    </td>
-                  </tr>
-                </Fragment>
-              );
-            })}
-          </tbody>
-        </table>
+                          <button className="btn btn-info text-white" onClick={this.handleDetails.bind(this, index)}>
+                            <i className="fa fa-edit"></i> Details</button>
+                        </td>
+                      </tr>
+                    </Fragment>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
