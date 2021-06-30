f66fa7902e757957fc4f7349aa86d6157fcb3a7a
dagbouj hatem <dagboujhatem@gmail.com>
2021-06-30 12:24:29 +0100

    weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export class Weather extends Component {
   state = {
     data: {},
+    isFetching: true,
   };
 
   componentDidMount() {
@@ -12,18 +13,25 @@ export class Weather extends Component {
       url: "http://api.weatherstack.com/current?access_key=3ad490972ca62027b8d67ce49fdb9370&query=tunisia",
     }).then((response) => {
       this.setState({
-        data: response,
-      });
-      console.log(this.state.data.data.location.country);
-    console.log(response);
-    }).catch(error => console.log(error));
+        data: response.data,
+        isFetching: false,
+      }); 
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   render() {
-      
-    return <div>
-        
-        </div>;
+    return(
+    <div>
+        <h1 className="text-center text-primary my-3">{!this.state.isFetching ? 
+        `Country : ${this.state.data.location.country}` : ''}
+        </h1>
+        <h1 className="text-center text-primary my-3">{!this.state.isFetching ? 
+        `Temperature : ${this.state.data.current.temperature}` : ''}
+        </h1>
+        <p className="text-center text-primary my-3 h4">{this.state.isFetching ? 'Fetching weather data ...' : ''}</p>
+    </div>);
   }
 }
 
